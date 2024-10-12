18bf298596fb3f5408eaf2bc53cf23133eee9062
Elina <mityaginaelina@yandex.ru>
2024-10-12 13:43:17 +0300

    Правки по ревью

--- page_urls.py
+++ page_urls.py
@@ -1,12 +1,12 @@
 BASE_URL = 'https://stellarburgers.nomoreparties.site/'
 
-register_url = BASE_URL + 'api/auth/register'
-user_url = BASE_URL + 'api/auth/user'
-user_login = BASE_URL + 'api/auth/user/login'
+register_url = f'{BASE_URL}api/auth/register'
+user_url = f'{BASE_URL}api/auth/user'
+user_login = f'{BASE_URL}api/auth/user/login'
 
-PROFILE_PAGE = "https://stellarburgers.nomoreparties.site/account/profile"
-FEED_PAGE = "https://stellarburgers.nomoreparties.site/feed"
-LOGIN_PAGE = "https://stellarburgers.nomoreparties.site/login"
-RECOVERY_PAGE = "https://stellarburgers.nomoreparties.site/forgot-password"
-ORDER_HISTORY = "https://stellarburgers.nomoreparties.site/account/order-history"
-RESET_PAGE = "https://stellarburgers.nomoreparties.site/reset-password"
+PROFILE_PAGE = f'{BASE_URL}account/profile'
+FEED_PAGE = f'{BASE_URL}feed'
+LOGIN_PAGE = f'{BASE_URL}login'
+RECOVERY_PAGE = f'{BASE_URL}forgot-password'
+ORDER_HISTORY = f'{BASE_URL}account/order-history'
+RESET_PAGE = f'{BASE_URL}reset-password'
